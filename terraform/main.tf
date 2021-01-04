a1960add9328274e1634c9efccd4b897cd4929e1
berbeha.olexandr <berbeha.olexandr@gmail.com>
2021-01-04 22:10:11 +0200

    add elk fwr

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,27 +14,58 @@ resource "google_compute_subnetwork" "project-subnet" {
 resource "google_compute_firewall" "allow-all-internal" {
   name    = "allow-all-internal"
   network = google_compute_network.project-network.id
+
   allow {
     protocol = "tcp"
     ports    = ["0-65535"]
   }
+
   allow {
     protocol = "udp"
     ports    = ["0-65535"]
   }
+
   allow {
     protocol = "icmp"
   }
+
   source_ranges = ["10.0.1.0/28"]
 }
 
 resource "google_compute_firewall" "allow-ssh" {
   name    = "allow-ssh"
   network = google_compute_network.project-network.id
+
   allow {
     protocol = "tcp"
     ports    = [22]
   }
 
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-elasticsearch" {
+  name    = "allow-elastic-search"
+  network = google_compute_network.project-network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = [9200]
+  }
+
+  target_tags   = ["elasticsearch"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-kibana" {
+  name    = "allow-kibana"
+  network = google_compute_network.project-network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = [5601]
+  }
+
+  target_tags   = ["kibana"]
   source_ranges = ["0.0.0.0/0"]
 }
