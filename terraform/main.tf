0ea483622cb44d34128a5cbb8bb127dc0c46451c
berbeha.olexandr <berbeha.olexandr@gmail.com>
2021-01-04 21:53:05 +0200

    add fwr

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,26 @@ resource "google_compute_subnetwork" "project-subnet" {
   name                     = "${var.subnet_name}-subnet"
   ip_cidr_range            = "10.0.1.0/28"
   private_ip_google_access = "true"
-  network                  = "${google_compute_network.project-network.id}"
+  network                  = google_compute_network.project-network.id
   region                   = "europe-north1"
 }
+
+resource "google_compute_firewall" "allow-all-internal" {
+  name = "allow-all-internal"
+  network = google_compute_network.project-network.id
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["10.0.1.0/28"]
+}
+
+
+
+
+
